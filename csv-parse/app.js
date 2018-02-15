2dcbf99474628e2944e8a80128fd77cb9b5a7d97
Victor Ian Bautista <victor@mexiton.com>
2018-02-15 10:11:12 +0800

    Use events

--- csv-parse/app.js
+++ csv-parse/app.js
@@ -1,3 +1,6 @@
 /*eslint valid-jsdoc: "off"*/
 /*eslint-env es6*/
 'use strict';
+
+const parser = require('./csv-parser');
+parser();
--- csv-parse/csv-parser.js
+++ csv-parse/csv-parser.js
@@ -1,3 +1,34 @@
 /*eslint valid-jsdoc: "off"*/
 /*eslint-env es6*/
 'use strict';
+
+const PromisedCsv = require('promised-csv');
+const handler = new PromisedCsv();
+const TIERLIMIT = 3;
+
+/**
+ *
+ * @param {string} file
+ */
+module.exports = function (file) {
+  let reqtList = [];
+
+  console.log('Called from anonymous function');
+
+  handler.on('row', function (data) {
+    // accept only row header + 3 levels
+    if (data.length !== TIERLIMIT) {
+      const row = {
+        id: data[0],
+        tier1: parseInt(data[0]),
+        tier2: parseInt(data[1]),
+        tier3: parseInt(data[2])
+      };
+      reqtList.push(row);
+    };
+  });
+
+  handler.on('done', function () {
+    return reqtList;
+  });
+};
